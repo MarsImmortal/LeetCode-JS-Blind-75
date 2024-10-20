e26cff8d76167794796bf9746498463fd44612bf
MarsImmortal <s222198007@deakin.edu.au>
2024-10-20 22:29:06 +1100

    add relevant comments

--- stack/validParentheses/stack.js
+++ stack/validParentheses/stack.js
@@ -1,26 +1,26 @@
 class Solution{
     isValid(s){
-        const stack = [];
-        const map = {
+        const stack = []; //stack ds (fifo) suppose to store only opening braces
+        const map = { //map init having opening paran with corresponding close paran key
             '}':'{',
             ']':'[',
             ')':'('
         }
-        for (const char of s){
-            const isBracket = char in map;
-            if (!isBracket){
+        for (const char of s){ //iterate through the string
+            const isBracket = char in map; //check wether the element is closing bracket
+            if (!isBracket){ //if not add it to stack 
                 stack.push(char);
-                continue; 
+                continue; //and continue to next iteration 
             }
 
-            const isEqual = stack[stack.length-1] === map[char];
-            if (isEqual) {
-                stack.pop();
-                continue;
+            const isEqual = stack[stack.length-1] === map[char]; 
+            if (isEqual) { //check the top-most element of the 
+                stack.pop();//stack contains the corresponding opening brace
+                continue; //move to next iteration
             }
-            
-            return false; 
+
+            return false; //else return false indicating the anamoly in the array
         }
-        return ! stack.length; 
+        return ! stack.length; //if stack's length is 0 return true else false
     }
 }
