937ea1d4943f5bf5869dd06845ed7760fa95bcc9
MarsImmortal <s222198007@deakin.edu.au>
2024-12-06 17:26:43 +1100

    sovle using recursion

---
+++ binarySearch/binarySearch/recursive.js
@@ -0,0 +1,12 @@
+class Solution{
+    binarySearch(l, r, nums, target){
+        let mid = l + Math.floor(r-l/2);
+        if (nums[mid] === target) return mid;
+        return (nums[mid]> target) ? this.binarySearch(l, mid-1, nums, target): this.binarySearch(mid+1, r, nums, target); 
+    }
+    search(nums, target){
+        return this.binarySearch(0, nums.length-1, nums, target);
+    }
+}
+//worse than itertative implementation on the basis of space complexity
+//time and space complexity O(log n) and O(log n)
